de88f4661e26bc614e16247012b886af25a104ef
Rakesh02005 <147812795+Rakesh02005@users.noreply.github.com>
2026-02-12 10:04:12 +0530

    Update 2.Monoalphabetic .c

--- 2.Monoalphabetic .c
+++ 2.Monoalphabetic .c
@@ -1,46 +1,16 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+#include<stdio.h>
+#include<string.h>
 
-int main() {
-    char plain[100], cipher[100], decrypted[100];
-    char key[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+int main(){
+    char p[100],k[]="QWERTYUIOPASDFGHJKLZXCVBNM",c[100];
     int i;
-
-    printf("Enter Plain Text (UPPERCASE): ");
-    scanf("%s", plain);
-
-    for(i = 0; plain[i] != '\0'; i++) {
-        if(isalpha(plain[i])) {
-            cipher[i] = key[plain[i] - 'A'];
-        } else {
-            cipher[i] = plain[i];
-        }
-    }
-    cipher[i] = '\0';
-
-    for(i = 0; cipher[i] != '\0'; i++) {
-        if(isalpha(cipher[i])) {
-            for(int j = 0; j < 26; j++) {
-                if(cipher[i] == key[j]) {
-                    decrypted[i] = j + 'A';
-                    break;
-                }
-            }
-        } else {
-            decrypted[i] = cipher[i];
-        }
-    }
-    decrypted[i] = '\0';
-
-    printf("\nCipher Text: %s", cipher);
-    printf("\nDecrypted Text: %s", decrypted);
-
-    return 0;
+    printf("Text: "); scanf("%s",p);
+    for(i=0;p[i];i++) c[i]=k[p[i]-65];
+    c[i]=0;
+    printf("Cipher: %s",c);
 }
 
 output:
-Enter Plain Text (UPPERCASE): HELLO
+Text: HELLO
+Cipher: ITSSG
 
-Cipher Text: ITSSG
-Decrypted Text: HELLO
